f9b3a8f55980e6a1e593647172e29f9d2b29c807
McgrathAaron <aaronnmcgrath@gmail.com>
2023-10-30 17:36:36 +0000

    fixed deploy line in viteconfig file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/",
+  base: "/guide-work-4/",
   resolve: {
     alias: [{find: "@", replacement: path.resolve(__dirname, 'src')},]
   },
